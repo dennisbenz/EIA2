8b9ab5761b8a741ac0ba9ac1a24be85abe8ab25f
sony <sony@Sanne.speedport.ip>
2018-10-28 12:32:09 +0100

    Aufgabe2 hochladen neu überarbeitet

--- Aufgabe2/Aufgabe2.js
+++ Aufgabe2/Aufgabe2.js
@@ -1,223 +1,41 @@
-var Unocards;
-(function (Unocards_1) {
-    function Unocards() {
-        let k0 = {
-            name: "0",
-            red: 1,
-            blue: 1,
-            green: 1,
-            yellow: 1
-        };
-        let k1 = {
-            name: "1",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let k2 = {
-            name: "2",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let k3 = {
-            name: "3",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let k4 = {
-            name: "4",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let k5 = {
-            name: "5",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let k6 = {
-            name: "6",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let k7 = {
-            name: "7",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let k8 = {
-            name: "8",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let k9 = {
-            name: "9",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let kr = {
-            name: "Reverse",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let kp2 = {
-            name: "Plus 2",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let ka = {
-            name: "Aussetzen",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-        let allKarten = [k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, kr, kp2, ka];
-        let s1 = 4;
-        let s2 = 4;
-        let c = "";
-        function random1(x) {
-            return Math.floor(Math.random() * Math.floor(x));
-        }
-        function placeDiv(_color, _v, _y) {
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "a" + _y);
-            document.getElementById("a" + _y).innerHTML += _v;
-            let s = div.style;
-            s.border = "thin solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = _color;
-            s.width = 100 + "px";
-            s.height = 200 + "px";
-            s.left = (_y + 0.2) * 110 + "px";
-            s.bottom = 40 + "px";
-            if (_color == "#000000") {
-                s.color = "white";
-            }
-            if (_color == "#0000ff") {
-                s.color = "white";
+/*Aufgabe: Aufgabe2
+Name: Dennis Vimon
+Matrikel:
+Datum: 26.10.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var UnoSpiel;
+(function (UnoSpiel) {
+    let colors = ["red", "blue", "green", "yellow"];
+    let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "<=>"];
+    let cards = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "yellow", value: "0" }, { color: "green", value: "0" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }];
+    for (let i = 0; i < colors.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            for (let k = 0; k < 2; k++) {
+                let card = { color: colors[i], value: values[j] };
+                cards.push(card);
             }
         }
-        let z;
-        let input = prompt("Anzahl der Karten auswaehlen");
-        z = Number(input);
-        for (let d = 0; d < z; d++) {
-            let l = random1(15);
-            if (l == 13 && s1 > 0) {
-                c = "#000000";
-                s1--;
-                placeDiv(c, "+4", d);
-                continue;
-            }
-            else if (l == 13 && s1 <= 0) {
-                d--;
-                continue;
-            }
-            else {
-                if (l == 14 && s2 > 0) {
-                    c = "#000000";
-                    s2--;
-                    placeDiv(c, "Farbauswahl", d);
-                    continue;
-                }
-                else if (l == 14 && s2 <= 0) {
-                    d--;
-                    continue;
-                }
-                else {
-                    let b = random1(4);
-                    switch (b) {
-                        case 0:
-                            c = "#ff0000";
-                            if (allKarten[l].red > 0) {
-                                placeDiv(c, allKarten[l].name, d);
-                                allKarten[l].red--;
-                                continue;
-                            }
-                        case 1:
-                            c = "#00ff00";
-                            if (allKarten[l].green > 0) {
-                                placeDiv(c, allKarten[l].name, d);
-                                allKarten[l].green--;
-                                continue;
-                            }
-                        case 2:
-                            c = "#0000ff";
-                            if (allKarten[l].blue > 0) {
-                                placeDiv(c, allKarten[l].name, d);
-                                allKarten[l].blue--;
-                                continue;
-                            }
-                        case 3:
-                            c = "#ffff00";
-                            if (allKarten[l].yellow > 0) {
-                                placeDiv(c, allKarten[l].name, d);
-                                allKarten[l].yellow--;
-                                continue;
-                            }
-                            else {
-                                d--;
-                                continue;
-                            }
-                    }
-                }
-            }
-        }
-        function Stapel() {
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "Kartenstapel");
-            document.getElementById("Kartenstapel").innerHTML += "Kartenstapel";
-            let s = div.style;
-            s.border = "solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = "#A04000";
-            s.width = 100 + "px";
-            s.height = 200 + "px";
-            s.left = 50 + "px";
-            s.top = 20 + "px";
-        }
-        function AblageStapel() {
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "Ablage");
-            document.getElementById("Ablage").innerHTML += "Ablage";
-            let s = div.style;
-            s.border = "solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = "#717D7E";
-            s.width = 100 + "px";
-            s.height = 200 + "px";
-            s.right = 50 + "px";
-            s.top = 20 + "px";
-        }
-        AblageStapel();
-        Stapel();
     }
-    document.addEventListener("DOMContentLoaded", (Unocards));
-})(Unocards || (Unocards = {}));
+    console.log(cards);
+    let handcards = [];
+    function Eingabe() {
+        var kartenanzahl = prompt("Kartenanzahl");
+        let n = parseInt(kartenanzahl);
+        for (let anz = n; anz > 0; anz--) {
+            let r = Math.floor(Math.random() * (cards.length - 1));
+            handcards.push(cards[r]);
+            cards.splice(r, 1);
+        }
+        for (let b = 0; b < handcards.length; b++) {
+            let div = document.createElement("div");
+            document.getElementById("Handkarten").appendChild(div);
+            div.innerHTML = handcards[b].value;
+            div.classList.add("Handkarten");
+            div.classList.add(handcards[b].color);
+        }
+    }
+    document.addEventListener("DOMContentLoaded", Eingabe);
+})(UnoSpiel || (UnoSpiel = {}));
 //# sourceMappingURL=Aufgabe2.js.map
--- Aufgabe2/Aufgabe2.ts
+++ Aufgabe2/Aufgabe2.ts
@@ -1,267 +1,50 @@
-namespace Unocards {
-
-    function Unocards(): void { 
-
-        interface Karte { 
-            name: string;
-            red: number;
-            blue: number;
-            green: number;
-            yellow: number;
-        }
-
-        let k0: Karte = {
-            name: "0",
-            red: 1,
-            blue: 1,
-            green: 1,
-            yellow: 1
-        };
-
-        let k1: Karte = {
-            name: "1",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let k2: Karte = {
-            name: "2",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let k3: Karte = {
-            name: "3",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let k4: Karte = {
-            name: "4",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let k5: Karte = {
-            name: "5",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let k6: Karte = {
-            name: "6",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let k7: Karte = {
-            name: "7",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let k8: Karte = {
-            name: "8",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let k9: Karte = {
-            name: "9",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let kr: Karte = {
-            name: "Reverse",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let kp2: Karte = {
-            name: "Plus 2",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let ka: Karte = {
-            name: "Aussetzen",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2
-        };
-
-        let allKarten: Karte[] = [k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, kr, kp2, ka]; 
-        let s1: number = 4 ;
-        let s2: number = 4;
-        let c: string = ""; 
-
-
-
-        function random1(x: number): number { 
-            return Math.floor(Math.random() * Math.floor(x));
-        }
-
-
-
-        function placeDiv(_color: string, _v: string, _y: number): void {
-            let div: HTMLDivElement = document.createElement("div");
-            document.body.appendChild(div);
-
-            div.setAttribute("id", "a" + _y);            
-            document.getElementById("a" + _y).innerHTML += _v;             
-            let s: CSSStyleDeclaration = div.style;
-            s.border = "thin solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = _color;
-            s.width = 100 + "px";
-            s.height = 200 + "px";
-            s.left = (_y + 0.2) * 110 + "px";
-            s.bottom = 40 + "px";
-            if (_color == "#000000") {
-                s.color = "white";
-            }
-            if (_color == "#0000ff") {
-                s.color = "white";
-            }
-
-
-        }
-
-        let z: number;
-        let input: string = prompt("Anzahl der Karten auswaehlen"); 
-        z = Number(input); 
-
-
-        for (let d: number = 0; d < z; d++) { 
-            let l: number = random1(15); 
-            if (l == 13 && s1 > 0) { 
-                c = "#000000"; 
-                s1--; 
-                placeDiv(c, "+4", d); 
-                continue; 
-            }
-
-            else if (l == 13 && s1 <= 0) { 
-                d--; 
-                continue;
-            }
-            else { 
-                if (l == 14 && s2 > 0) {
-                    c = "#000000";
-                    s2--;
-                    placeDiv(c, "Farbauswahl", d);
-                    continue;
-                }
-                else if (l == 14 && s2 <= 0) {
-                    d--;
-                    continue;
-                }
-                else {
-                    let b: number = random1(4); 
-                    switch (b) { 
-                        case 0:
-                            c = "#ff0000";
-                            if (allKarten[l].red > 0) {
-                                placeDiv(c, allKarten[l].name, d); 
-                                allKarten[l].red--;
-                                continue;
-                            }
-                        case 1:
-                            c = "#00ff00";
-                            if (allKarten[l].green > 0) {
-                                placeDiv(c, allKarten[l].name, d);
-                                allKarten[l].green--;
-                                continue;
-                            }
-
-                        case 2:
-                            c = "#0000ff";
-                            if (allKarten[l].blue > 0) {
-                                placeDiv(c, allKarten[l].name, d);
-                                allKarten[l].blue--;
-                                continue;
-                            }
-
-                        case 3:
-                            c = "#ffff00";
-                            if (allKarten[l].yellow > 0) {
-                                placeDiv(c, allKarten[l].name, d);
-                                allKarten[l].yellow--;
-                                continue;
-                            }
-                            else {
-                                d--;
-                                continue;
-                            }
-
-                    }
-
-                }
-
-            }
-        }
-        function Stapel(): void { 
-            let div: HTMLElement = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "Kartenstapel");
-            document.getElementById("Kartenstapel").innerHTML += "Kartenstapel";
-            let s = div.style;
-            s.border = "solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = "#A04000";
-            s.width = 100 + "px";
-            s.height = 200 + "px";
-            s.left = 50 + "px";
-            s.top = 20 + "px";
-        }
-
-        function AblageStapel(): void {
-            let div: HTMLElement = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "Ablage");
-            document.getElementById("Ablage").innerHTML += "Ablage";
-            let s = div.style;
-            s.border = "solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = "#717D7E";
-            s.width = 100 + "px";
-            s.height = 200 + "px";
-            s.right = 50 + "px";
-            s.top = 20 + "px";
-        }
-
-        AblageStapel();
-        Stapel();
+/*Aufgabe: Aufgabe2
+Name: Dennis Vimon
+Matrikel: 
+Datum: 26.10.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+namespace UnoSpiel {
+    interface Unocard {
+        color: string;
+        value: string;
     }
 
-    document.addEventListener("DOMContentLoaded", (Unocards)) ;
+    let colors: string[] = ["red", "blue", "green", "yellow"];
+    let values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "<=>"];
+    let cards: Unocard[] = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "yellow", value: "0" }, { color: "green", value: "0" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }];
 
+    for (let i: number = 0; i < colors.length; i++) {
+        for (let j: number = 0; j < values.length; j++) {
+            for (let k: number = 0; k < 2; k++) {
+                let card: Unocard = { color: colors[i], value: values[j] };
+                cards.push(card);
+            }
+        }
+    }
+    console.log(cards);
 
+    let handcards: Unocard[] = [];
+
+    function Eingabe(): void {
+        var kartenanzahl: string = prompt("Kartenanzahl");
+        let n: number = parseInt(kartenanzahl);
+
+        for (let anz: number = n; anz > 0; anz--) {
+            let r: number = Math.floor(Math.random() * (cards.length - 1));
+            handcards.push(cards[r]);
+            cards.splice(r, 1);
+        }
+
+        for (let b: number = 0; b < handcards.length; b++) {
+            let div: HTMLElement = document.createElement("div");
+            document.getElementById("Handkarten").appendChild(div);
+            div.innerHTML = handcards[b].value;
+            div.classList.add("Handkarten");
+            div.classList.add(handcards[b].color);
+        }
+        
+    }
+    document.addEventListener("DOMContentLoaded", Eingabe);
 }
